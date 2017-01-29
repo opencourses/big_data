e8af0b5115cc773d910fcb4ea657c1b780afedd6
Antonio Langiu <antonio.langiu@studenti.polito.it>
2017-01-29 18:30:08 +0100

    fix files in hadoop template

--- templates/proj/hadoop/run.sh
+++ templates/proj/hadoop/run.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -e 
 
+base_dir=$(cd `dirname "$0"` && pwd -P)
+
 # This script moves the files in the filesystem and then run hadoop
 # Remove folders of the previous run
-hdfs dfs -rm -r /ex_name_data
-hdfs dfs -rm -r /ex_name_out
+hdfs dfs -ls /ex_name_data > /dev/null && hdfs dfs -rm -r /ex_name_data
+hdfs dfs -ls /ex_name_out > /dev/null && hdfs dfs -rm -r /ex_name_out
 
 # Put input data collection into hdfs
-hdfs dfs -put /ex_name_data
+hdfs dfs -copyFromLocal "$base_dir/ex_name_data" /ex_name_data
 
 
 # Run application
-hadoop jar target/ex_name-1.0.0.jar it.polito.bigdata.hadoop.DriverImpl 1 ex_name_data/document.txt  ex_name_out
+hadoop jar "$base_dir/target/ex_name-1.0.0.jar" it.polito.bigdata.hadoop.DriverImpl 1 ex_name_data/document.txt  ex_name_out
 
 
 
