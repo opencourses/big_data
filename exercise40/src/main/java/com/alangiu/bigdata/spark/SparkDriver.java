4f941f121a2d4c325c9bfae05bb5dc187f3e27c6
Antonio Langiu <antonio.langiu@studenti.polito.it>
2017-02-02 09:16:55 +0100

    complete exercise 40 and remove 41

--- exercise40/src/main/java/com/alangiu/bigdata/spark/SparkDriver.java
+++ exercise40/src/main/java/com/alangiu/bigdata/spark/SparkDriver.java
@@ -2,6 +2,13 @@ package com.alangiu.bigdata.spark;
 
 import org.apache.spark.api.java.*;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
+
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.spark.SparkConf;
 
 public class SparkDriver {
@@ -17,16 +24,44 @@ public class SparkDriver {
 
 		JavaRDD<String> readRDD = sc.textFile(inputPath);
 
-		JavaRDD<String> finalRDD = readRDD.filter(new Function<String, Boolean>() {
+		JavaPairRDD<Integer, Tuple2<Integer,String>> windowsItemRDD = 
+				readRDD.flatMapToPair(new PairFlatMapFunction<String, Integer, Tuple2<Integer, String>>() {
+
+					@Override
+					public Iterable<Tuple2<Integer, Tuple2<Integer, String>>> call(String arg0) throws Exception {
+						List<Tuple2<Integer, Tuple2<Integer, String>>> list = new ArrayList<>();
+						String[] s = arg0.split(",");
+						Integer timestamp = Integer.parseInt(s[0]);
+						list.add(new Tuple2<Integer, Tuple2<Integer, String>>(timestamp, new Tuple2<Integer, String>(timestamp, s[1])));
+						list.add(new Tuple2<Integer, Tuple2<Integer, String>>(timestamp-60, new Tuple2<Integer, String>(timestamp-60, s[1])));
+						list.add(new Tuple2<Integer, Tuple2<Integer, String>>(timestamp-120, new Tuple2<Integer, String>(timestamp-120, s[1])));
+						return list;
+					}
+		});
+		
+		windowsItemRDD.groupByKey().filter(new Function<Tuple2<Integer, Iterable<Tuple2<Integer, String>>>, Boolean>() {
 
 			@Override
-			public Boolean call(String arg0) throws Exception {
-				return true;
+			public Boolean call(Tuple2<Integer, Iterable<Tuple2<Integer, String>>> arg) throws Exception {
+				int timestamp = arg._1;
+				int temp1 = 0;
+				int temp2 = -1;
+				int temp3 = -2;
+				for (Tuple2<Integer, String> i:arg._2) {
+					if (i._1 == timestamp) {
+						temp1 = i._1;
+					} else if (i._1 == timestamp+60) {
+						temp1 = i._1;
+					} else if (i._1 == timestamp+120) {
+						temp1 = i._1;
+					}
+				}
+				if (temp3 >= temp2 && temp2 >= temp1) {
+					return true;
+				}
+				return false;
 			}
-
-		});
-
-		finalRDD.saveAsTextFile(outputPath);
+		}).saveAsTextFile(outputPath);
 
 		sc.close();
 	}
